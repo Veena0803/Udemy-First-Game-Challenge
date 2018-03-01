194fe5a618efdf0b81b3c84199951fd05fbc3925
Veena0803 <iyerveena08@gmail.com>
2018-03-01 08:24:50 -0500

    Lecture:Variables

---
+++ script.js
@@ -0,0 +1,14 @@
+// Lecture: variables
+/*
+var name = 'John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = true;
+console.log(fullAge);
+*/
