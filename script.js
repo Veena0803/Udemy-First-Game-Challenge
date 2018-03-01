40d950003f98bcb4b2a767149f18c3d1b45d46fc
Veena0803 <iyerveena08@gmail.com>
2018-03-01 08:34:11 -0500

    Lecture:Functions

--- script.js
+++ script.js
@@ -128,3 +128,33 @@ switch (job) {
         console.log('John does something else.');
 }
 */
+
+// Lecture: Functions
+
+/*
+function calculateAge(yearOfBirth) {
+    var age = 2016 - yearOfBirth;
+    return age;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1969);
+var ageMary = calculateAge(1948);
+
+
+function yearsUntilRetirement(name, year) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    
+    if (retirement >= 0) {
+        console.log(name + ' retires in ' + retirement + ' years.');
+    } else {
+        console.log(name + ' is already retired.');
+    }
+}
+
+
+yearsUntilRetirement('John', 1990);
+yearsUntilRetirement('Mike', 1969);
+yearsUntilRetirement('Mary', 1948);
+*/
